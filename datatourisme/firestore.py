be8ac9a4305d2e89edbbe0e4b5b165d60205b028
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-11-03 20:27:41 +0100

    first data added to Firestore

--- datatourisme/firestore.py
+++ datatourisme/firestore.py
@@ -4,7 +4,7 @@ from firebase_admin import firestore
 # URL du fichier JSON-LD
 url = "https://diffuseur.datatourisme.fr/webservice/49eebc93390a819eb8c2a0f95a150916/93cac18d-5c3e-4101-b0f5-db0c94423c88"
 
-cred =credentials.Certificate('festalocal-bd4613184dd.json')
+cred =credentials.Certificate('festalocal-bd4613184dd8.json')
 
 app = firebase_admin.initialize_app(cred)
 
